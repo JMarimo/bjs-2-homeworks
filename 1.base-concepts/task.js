613bbe2ee0a7e3c48edfc51a96ed4b79582f40b4
Jane Marimo <jenny.bulatova@gmail.com>
2023-06-22 20:02:47 +0300

    Задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  const d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+      return arr;
+  }
+
+else if (d = 0) {
+      arr.push(-b / (2 * a));
+      return arr;
+  }
+ 
+  else if (d > 0) {
+      arr.push((-b + Math.sqrt(d)) / (2 * a));
+      arr.push((-b - Math.sqrt(d)) / (2 * a));
+      return arr;
+  }
+};
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (isNaN(percent) || isNaN(contribution)  ||  isNaN(amount)  || isNaN(countMonths)  ) {
+      return false;
+    }
+    let p = 1 / 12 / 100 * percent;
+    let body = amount - contribution;
+    let monthlyPayment = body * (p + (p / (((1 + p) ** countMonths) - 1)));
   
-}
+    let clientTotalAmount = monthlyPayment * countMonths;
+  
+    clientTotalAmount = Math.round(clientTotalAmount * 100) / 100;
+  
+    return clientTotalAmount;
+  };
