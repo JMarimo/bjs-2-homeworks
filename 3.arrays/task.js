66cc48fb9df61d7a851567a3e99863404e79117a
Jane Marimo <jenny.bulatova@gmail.com>
2023-06-29 13:15:45 +0300

    Задание выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,4 +1,3 @@
-function compareArrays(arr1, arr2) {
     function compareArrays(arr1, arr2) {
         if(arr1.length != arr2.length) {
           return false;
@@ -7,23 +6,7 @@ function compareArrays(arr1, arr2) {
             return elem === arr2[index];
          });
       }
-  }
+  
 
 function getUsersNamesInAgeRange(users, gender) {
-    function getUsersNamesInAgeRange(users, gender) {
-        if (!users || !Array.isArray(users) || users.length === 0 || !gender || typeof gender !== 'string') {
-        return 0;
-        }
-        
-        const filteredUsers = users.filter(user => user.gender === gender);
-        if (filteredUsers.length === 0) {
-        return 0;
-        }
-        
-        const totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
-        const averageAge = totalAge / filteredUsers.length;
-        
-        return averageAge;
-        }
-  
 }
