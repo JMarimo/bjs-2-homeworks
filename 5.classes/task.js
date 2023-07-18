3a811eeb8ddaff0c1a8e806ddf0bc390453febad
Jane Marimo <jenny.bulatova@gmail.com>
2023-07-18 13:47:25 +0300

    Второе задание выполнено

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -8,7 +8,7 @@ class PrintEditionItem {
     }
 
     fix() {
-        return this.state = this.state*1,5;
+        return this.state = this.state*1.5;
     }
 
     set state(stateNew) {
@@ -59,5 +59,38 @@ class DetectiveBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
         this.type = "detective";
+    }
+}
+
+class Library {
+    constructor(name, books) {
+        this.name = name,
+        this.books = [];
+    }
+}
+
+addBook(book) {
+    if (book.state > 30) {
+        this.books.push(book);
+    }       
+}
+
+findBookBy(type, value) {
+    let findBook = this.books.find(book => book[type] === value);
+        if (typeof findBook === 'object') {
+            return findBook;
+        } else {
+            return null;
+        } 
+    }
+
+giveBookByName(bookName) {
+    let givebook = this.book.find(book => book.name === bookName);
+    if (typeof giveBook === 'object') {
+        let index = this.books.indexOf(giveBook);
+        this.books.splice(index, 1);
+        return giveBook;
+    } else {
+        return null;
     }
 }
